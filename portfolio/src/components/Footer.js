be2cc22e7219ad6aceea8dabf08aee4acaefd567
Atina <kellieatina@gmail.com>
2025-06-23 09:24:27 +0300

    Add Skills section styles and implement Footer component with social links and contact info

--- portfolio/src/components/Footer.js
+++ portfolio/src/components/Footer.js
@@ -1,1 +1,32 @@
-// Create your Footer component here
+import { FaFacebook, FaTwitter, FaInstagram } from "react-icons/fa";
+
+const Footer = () => {
+  return (
+    <footer id="footer" className="footer">
+      <div className="footer-contact-info">
+        <h1 className="footer-heading">Connect With Me</h1>
+        <p className="footer-contact-access">Email: test@educative.io</p>
+        <p className="footer-contact-access">Mobile: 1234 1234 1234</p>
+      </div>
+      <div>
+        <h1>Social Links</h1>
+        <div className="social-icons">
+          <a href="https://www.educative.io/">
+            <i></i><FaFacebook /><i></i>
+            <i></i>
+          </a>
+          <a href="https://www.educative.io/">
+            <i></i><FaInstagram /><i></i>
+            <i></i>
+          </a>
+          <a href="https://www.educative.io/">
+            <i></i><FaTwitter /><i></i>
+            <i></i>
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
